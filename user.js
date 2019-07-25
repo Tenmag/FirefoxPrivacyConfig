66b5cea4a3c02e53435ab8e83875b4c301764a6f
Tenmag <39343665+Tenmag@users.noreply.github.com>
2019-07-25 13:51:46 +0200

    commit

--- user.js
+++ user.js
@@ -130,6 +130,10 @@ user_pref("network.trr.bootstrapAddress","146.185.167.43"); // web: https://secu
 user_pref("network.security.esni.enabled",true); // FF65 Encrypted SNI
 //
 user_pref("browser.link.open_newwindow.restriction",0); // Divert all links according to browser.link.open_newwindow.
+user_pref("browser.link.open_newwindow",3); // Default 3: same window new tab.
+user_pref("browser.link.open_newwindow.disabled_in_fullscreen",true);
+user_pref("dom.vr.oculus.enabled",false); // facebook-samsung VR off
+user_pref("dom.vr.enabled",false);
 user_pref("extensions.pocket.enabled",false);
 user_pref("media.eme.enabled",false); // disable DRM
 user_pref("social.enabled",false);
