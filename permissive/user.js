7a9451783caa7266c247354eb118b1713277339d
Tenmag <39343665+Tenmag@users.noreply.github.com>
2021-03-24 09:04:00 +0100

    commit

--- permissive/user.js
+++ permissive/user.js
@@ -197,8 +197,8 @@ user_pref("network.proxy.type",0);
 // user_pref("browser.display.use_document_fonts",0); // websites must use/list default fonts only
 // user_pref("browser.autofocus",false); // big-tech-co aggressive hack to manipulate user into something
 
-// user_pref("general.useragent.override","Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0");
-// user_pref("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324 Safari/537.36");
-// user_pref("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0");
+// user_pref("general.useragent.override","Mozilla/5.0 (X11; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0");
+// user_pref("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389 Safari/537.36");
+// user_pref("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0");
 // user_pref("network.http.useragent.global_override","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0"); // Basilisk/PaleMoon browser
 
